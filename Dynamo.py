db79cd6abefa7674f024ed04b144413f7e2f45e8
zappi-tkn <37334273+zappi-tkn@users.noreply.github.com>
2018-03-24 00:22:51 +1100

    Create Dynamo.py

---
+++ Dynamo.py
@@ -0,0 +1,87 @@
+from string import ascii_uppercase
+import random
+
+def Diana(Text, Key):
+    return ascii_uppercase[(25 - ord(Key) - ord(Text)) % 26]
+
+def CodeGroup(Text, Size):
+        return " ".join("".join(Text[i:i+Size]) for i in range(0, len(Text), Size))
+
+def MakeKeystream(Seed):
+    RNG = list("".join(Seed)) + [''] * len(Text)
+    for x in range(len(Text)):
+        RNG[x+len(Seed)]=(Diana(RNG[x],RNG[x+1]))
+    RNG = RNG[len(Seed):]
+    return "".join(RNG)
+   
+def OnlyAZ(Text):
+    Text=Text.upper()
+    NText=""
+    for Char in Text:
+        if Char in ascii_uppercase:
+            NText += Char
+    return(NText)
+
+def MakeSeed(Seed,Length):
+    #Making Seed. If you want a custom seed, assign it as a string to Seed at the same length as Init and Password.
+    Seed = list(Seed)
+    if Seed == []:
+        for x in range(Length):
+            Seed.append(random.choice(ascii_uppercase))
+        return "".join(Seed)
+    else:
+        return "".join(Seed)
+
+def MakePassword(Init1,Init2):
+    #Making Password (finding trigraphs of Init1 and Init2
+    if len(Init1) == len(Init2):
+        for x in range(len(Init1)):
+            Password.append(Diana(Init1[x],Init2[x]))
+    else:
+        print("Inits not of the same length, quitting...")
+    return "".join(Password)
+
+def MakeMask(Password,Seed):
+    for x in range(len(Seed)):
+        Mask.append(Diana(Password[x], Seed[x]))
+    return "".join(Mask)
+
+def Encrypt(Mask,Text,Keystream):
+    for x in range(len(Text)):
+        Ciphertext.append(Diana(Text[x],Keystream[x]))
+    return Ciphertext
+
+
+Alphabet1 = list("BULSKCQHTGYOVPFZRMEJAWDNXI")
+Alphabet2 = list("ZTNQCOFEAGDMWHBRLKISXJPVYU")
+
+Length = 9
+Location = 17
+
+Password = []
+Mask = []
+Ciphertext = []
+
+Text = OnlyAZ("All human beings are born free and equal in dignity and rights. They are endowed with reason and conscience and should act towards one another in a spirit of brotherhood.")
+
+#Making Inits under chosen starting location and length
+Init1 = Alphabet1[Location:Location+Length]
+Init2 = Alphabet2[Location:Location+Length]
+
+Password = MakePassword(Init1,Init2)
+Seed = MakeSeed("KJVAOZNYR",Length)
+Mask = MakeMask(Password,Seed)
+Keystream = MakeKeystream(Seed)
+Ciphertext = Encrypt(Mask,Text,Keystream)
+
+
+print("Init1:      "+"".join(Init1))
+print("Init2:      "+"".join(Init2))
+print("")
+print("Password:   "+Password)
+print("Seed:       "+Seed)
+print("Mask:       "+Mask)
+print("")
+print("Input:      "+CodeGroup((Text),5))
+print("Keystream:  "+CodeGroup(Keystream,5))
+print("Output:     "+CodeGroup("".join(Ciphertext),5))
